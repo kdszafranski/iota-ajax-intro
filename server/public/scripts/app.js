0247ccb3b5d2c469464220199c811c449599a2be
Kris Szafranski <kdszafranski@gmail.com>
2016-07-20 10:22:16 -0500

    updated for omicron data

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,30 +1,23 @@
 $(document).ready(function() {
-    //loadData();
+  $('#load-data').on('click', loadData);
 
-    $('#load-data').on('click', loadData);
-});
-
-function loadData() {
+  function loadData() {
     $.ajax({
-        type: 'GET',
-        url: '/data',
-        success: function(data) {
-            //console.log(data);
-            $.each(data.people, function(i, person) {
-                $('#ajax-data').append('<div class="person"></div>');
-                var $el = $('#ajax-data').children().last();
-                $el.append('<h2>' + person.name + '</h2>');
-                $el.append('<p>' + person.position + '</p>');
-                $el.append('<p>' + person.location + '</p>');
-                $el.append('<img src="' + person.imageURL +'" />');
-            });
-        },
-        error: function() {
-            // do logic here on fail
-            console.log('oops, error!');
-        },
-        timeout: function() {
-
+      type: 'GET',
+      url: '/data',
+      success: function(response) {
+        console.log(response.people);
+        for(var i = 0; i < response.people.length; i++) {
+          $('#ajax-data').append('<div class="person"></div>');
+          var $el = $('#ajax-data').children().last();
+          $el.append('<h2>' + response.people[i].name + '</h2>');
+          $el.append('<img src="' + response.people[i].imageURL + '" />');
         }
+      },
+      error: function() {
+        console.log('Error with request');
+      }
+
     });
-}
+  }
+});
