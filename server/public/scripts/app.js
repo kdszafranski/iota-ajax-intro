599eb4384e5fd775a3248d2b3e9355f6aa8fbdc2
Kris Szafranski <kdszafranski@gmail.com>
2017-02-17 12:25:46 -0600

    rewind to lecture start

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,23 +1,3 @@
 $(document).ready(function() {
-  $('#load-data').on('click', loadData);
-
-  function loadData() {
-    $.ajax({
-      type: 'GET',
-      url: '/data',
-      success: function(response) {
-        console.log(response.people);
-        for(var i = 0; i < response.people.length; i++) {
-          $('#ajax-data').append('<div class="person"></div>');
-          var $el = $('#ajax-data').children().last();
-          $el.append('<h2>' + response.people[i].name + '</h2>');
-          $el.append('<img src="' + response.people[i].imageURL + '" />');
-        }
-      },
-      error: function() {
-        console.log('Error with request');
-      }
-
-    });
-  }
+  console.log('jquery running!');
 });
